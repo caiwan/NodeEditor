659bd30c57a0f2237f5bb5e66782bf2a7494aa75
Caiwan <caiwan.lupus@gmail.com>
2019-09-23 14:31:17 +0200

    WIP

--- create_makefiles.sh
+++ create_makefiles.sh
@@ -1,5 +1,10 @@
-mkdir -p build/
-cd build 
-cmake -DCMAKE_INSTALL_PREFIX=deploy/ -DCMAKE_PREFIX_PATH=$QT5DIR -G "Unix Makefiles" ../ 
-cd ../
+#!/bin/bash
+DIR=./build
+[ -d "$DIR" ] && [ -f "$DIR/CmakeCache.txt" ] && rm "$DIR/CmakeCache.txt" || mkdir -p $DIR
+cd $DIR
 
+cmake -DCMAKE_INSTALL_PREFIX=deploy/ -DCMAKE_PREFIX_PATH=$QT5DIR/lib/cmake -G "Unix Makefiles" ../
+
+cmake --build ./ --target ExternalDependencies
+
+cd ../
--- create_xcode.sh
+++ create_xcode.sh
@@ -1,5 +1,11 @@
-mkdir -p xcode/
-cd xcode 
-cmake -DCMAKE_INSTALL_PREFIX=deploy/ -DQt5_DIR=$QT5DIR -DCMAKE_PREFIX_PATH=$QT5DIR -G "Xcode" ../ 
-cd ../
+#!/bin/bash
+DIR=./xcode
+[ -d "$DIR" ] && [ -f "$DIR/CmakeCache.txt" ] && rm $DIR/CmakeCache.txt || mkdir -p $DIR
+cd $DIR
 
+cmake -DCMAKE_INSTALL_PREFIX=deploy/ -DQT5DIR=$QT5DIR -DCMAKE_PREFIX_PATH=$QT5DIR -G "Xcode" ../
+
+cmake --build ./ --target ExternalDependencies --config Debug
+cmake --build ./ --target ExternalDependencies --config MinSizeRel
+
+cd ../
